ea0fa158e40157a1ce258ceac6c58cb1abe4cd8a
ProB1 <50064486+iProB1@users.noreply.github.com>
2022-12-30 17:17:56 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include "webhook.h"
+
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if (start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
+int main() {
+  SetConsoleTitleA("Discord Webhook Sender");
+  printf("made by ProB1#0100\n");
+  DWHook webhook;
+  std::string text = webhook.normal;
+  replace(text, "username123", "New Username");
+  replace(text, "text", "New Text");
+  webhook.send(text);
+  std::string text2 = webhook.description;
+  replace(text2, "username123", "New Username 2");
+  replace(text2, "descriptiontitle", "New Description Title");
+  replace(text2, "descriptiontext", "New Description Text");
+  webhook.send(text2);
+  printf("if you got error about sending webhook its your text issue need to work well\n");
+  return
+}
